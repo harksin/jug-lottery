c3f5139b3654837f045642a7dee44dfa70e8527b
harksin <r.dardie@gmail.com>
2016-10-12 00:33:03 +0200

    feat(random) trusted provider

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Nbrite = require('nbrite');
 const express = require('express');
+const rp = require('request-promise');
 
 const nbrite = new Nbrite({'token': process.env.EVENTBRITE_TOKEN});
+
 
 const range = (start, end) => {
   let pages = []
@@ -20,25 +22,24 @@ const getAttendees = (event_id) => {
     )
 }
 
-const findWinner = (attendees, nbWinner) => {
-  // Deception no immutable :'(
-  let localAttendees = attendees.slice(0)
-  const internalFindWinner = (acc, nbWinner) => {
-    if (nbWinner == 0) {
-      return acc
-    }
-    let winner = localAttendees.splice(Math.floor(Math.random()*localAttendees.length), 1)
-    return internalFindWinner(acc.concat(winner), nbWinner - 1)
-  }
-  return internalFindWinner([], nbWinner)
+const finWinnerWithTrustedRandom = (attendees, nbWinner) => {
+  return rp(`https://www.random.org/integers/?num=${nbWinner}&min=0&max=${attendees.length}&col=1&base=10&format=plain&rnd=new`)
+      .then(function (body) {
+       console.log(body);
+        return body.split("\n")
+                  .filter((element)=>{return element.length > 0})
+                  .map(Number)
+                  .map((index)=> { return attendees[index]})
+      })
 }
+
 
 const app = express();
 
 
 app.get('/:eventId/:nbWinners', function(req, res){
   getAttendees(req.params.eventId)
-    .then(attendees => findWinner(attendees, req.params.nbWinners))
+    .then(attendees => finWinnerWithTrustedRandom(attendees, req.params.nbWinners))
     .then(winners => winners.map(({profile: {first_name: first_name, last_name: last_name}}) => { return {'Winner': `${first_name} ${last_name}`}}))
     .then(winners => res.send(winners))
 });
